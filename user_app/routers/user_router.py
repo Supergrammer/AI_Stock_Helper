858fab921b77a46b1e226a3dde7188bbc239a067
supergrammer <supergrammer@naver.com>
2021-06-16 17:26:19 +0900

    [Feat] 유저 라우터 및 서비스 추가, #5

--- user_app/routers/user_router.py
+++ user_app/routers/user_router.py
@@ -1,23 +1,42 @@
-from fastapi import APIRouter, Depends, HTTPException
+from typing import List, Optional
+
+from fastapi import APIRouter, HTTPException, status
 from ..schemas import user_schema
-from ..services import user_service
+from ..services import user_service, user_auth_service
 
 router = APIRouter(
     prefix="/user",
     tags=["user"],
 )
 
-@router.get("/")
-def get_all_users():
-    return user_service.get_all_user()
+# TODO: async await
+@router.get("/get", response_model=user_schema.UserGet)
+async def get_user_by_id_or_email(id: Optional[int] = None, email: Optional[str] = None):
+    if not bool(id) ^ bool(email):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+    else:
+        user = user_service.get_user_by_id(id=id) if id \
+            else user_service.get_user_by_email(email=email)
 
-# @router.post("/signup", response_model=user_schema.User_response)
-# def create_user(user: user_schema.UserCreate):
-#     db_user = user_service.get_user_by_email(email=user.email)
-#     if db_user:
-#         raise HTTPException(status_code=400, detail="Email already exist...")
-#     return user_service.create_user
+        if not user:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        return user
 
-@router.post("/signup")
-def create_user(user: user_schema.User_request):
-    return user_service.create_user(user=user)
+@router.get("/get/all", response_model=List[user_schema.UserGet])
+async def get_all_users(skip: Optional[int] = None, limit: Optional[int] = None):
+    return user_service.get_all_users()
+
+@router.post("/signup", response_model=user_schema.UserGet)
+async def create_user(user: user_schema.UserCreate):
+    user.password = user_auth_service.get_hashed_password(user.password)
+    db_user = user_service.get_user_by_email(email=user.email)
+    if db_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
+    return user_service.create_user(user)
+
+@router.delete("/signout", response_model=user_schema.UserGet)
+async def delete_user(user: user_schema.UserDelete):
+    db_user = user_service.get_user_by_email(email=user.email)
+    if not db_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User does not exist")
+    return user_service.delete_user(user)
--- user_app/services/user_service.py
+++ user_app/services/user_service.py
@@ -1,37 +1,31 @@
-from ..schemas.user_schema import User_request, User_response
-from sqlalchemy import schema
 from sqlalchemy.orm import Session
 
 from ..config.database import get_db
 
 from ..models.user_model import User
-from ..schemas.user_schema import *
+from ..schemas import user_schema
 
-def get_user(id: int, db: Session = get_db()):
+db: Session = get_db()
+
+def get_user_by_id(id: int):
     return db.query(User).filter(User.id == id).first()
 
-def get_user_by_email(email: str, db: Session = get_db()):
+def get_user_by_email(email: str):
     return db.query(User).filter(User.email == email).first()
 
-def get_all_user(skip: int = 0, limit: int = 100, db: Session = get_db()):
+def get_all_users(skip: int = 0, limit: int = 100):
     return db.query(User).offset(skip).limit(limit).all()
 
-# def create_user(user: User_request, db: Session = get_db()):
-#     # db_user = User_request(email=user.email, password=user.password, username=user.username)
-#     db_user = User(**user.dict())
+def create_user(user: user_schema.UserCreate):
+    user = User(**user.dict())
 
-#     db.add(db_user)
-#     db.commit()
-#     db.refresh(db_user)
-#     return db_user
+    db.add(user)
+    db.commit(); db.refresh(user)
+    return user
 
-def create_user(user: User_request, db: Session = get_db()):
-    db_user = User(
-        email=user.email,
-        password=user.password,
-        username=user.username
-    )
-    db.add(db_user)
+def delete_user(user: user_schema.UserDelete):
+    user = db.query(User).filter(User.email == user.email and User.password == user.password).first()
+
+    db.delete(user)
     db.commit()
-    db.refresh(db_user)
-    return db_user
+    return user

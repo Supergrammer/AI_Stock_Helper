3fd774601dd3d94174d2aadf5db95ecb9fb90e19
Supergrammer <supergrammer@naver.com>
2021-07-10 14:04:09 +0900

    [Feat] User service, router 기능 추가, #5

--- user_app/routers/user_router.py
+++ user_app/routers/user_router.py
@@ -12,6 +12,16 @@ router = APIRouter(
 )
 
 # TODO: async await
+# NOTE: /user/signup : 회원가입
+@router.post("/signup", response_model=user_schema.UserRead)
+async def create_user(user: user_schema.UserCreateOrUpdate):
+    if not user.password:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Password has not been entered")
+    if user_service.get_user_by_email(email=user.email):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
+    return user_service.create_user(user)
+
+# NOTE: /user/get : id 또는 email 로부터 유저 정보를 반환
 @router.get("/get", response_model=user_schema.UserRead)
 async def get_user_by_id_or_email(id: Optional[UUID] = None, email: Optional[str] = None):
     if not bool(id) ^ bool(email):
@@ -24,20 +34,23 @@ async def get_user_by_id_or_email(id: Optional[UUID] = None, email: Optional[str
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
         return user
 
+# NOTE: /user/get/all : 모든 유저의 정보를 반환
 @router.get("/get/all", response_model=List[user_schema.UserRead])
 async def get_all_users(skip: Optional[int] = None, limit: Optional[int] = None):
     return user_service.get_all_users(skip=skip, limit=limit)
 
-@router.post("/signup", response_model=user_schema.User)
-async def create_user(user: user_schema.UserCreate):
-    db_user = user_service.get_user_by_email(email=user.email)
-    if db_user:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
-    return user_service.create_user(user)
-
-@router.delete("/signout", response_model=user_schema.UserRead)
-async def delete_user(user: user_schema.UserDelete):
+# NOTE: /user/update : 유저 정보 업데이트
+@router.put("/update", response_model=user_schema.UserRead)
+async def update_user(user: user_schema.UserCreateOrUpdate):
     db_user = user_service.get_user_by_email(email=user.email)
     if not db_user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User does not exist")
-    return user_service.delete_user(user)
+    return user_service.update_user(user)
+
+# NOTE: /user/signout : email 을 인자로 받아 회원탈퇴
+@router.delete("/signout", response_model=user_schema.UserRead)
+async def delete_user(email: str):
+    db_user = user_service.get_user_by_email(email)
+    if not db_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User does not exist")
+    return user_service.delete_user(email)
--- user_app/services/user_service.py
+++ user_app/services/user_service.py
@@ -1,6 +1,5 @@
 from uuid import UUID
 
-from fastapi import HTTPException, status, Security
 from sqlalchemy.orm import Session
 
 from ..config.database import get_db
@@ -10,16 +9,7 @@ from ..models.user_model import User
 from ..models.password_model import Password
 from ..schemas import user_schema
 
-def get_user_by_id(id: UUID, db: Session = get_db()):
-    return db.query(User).filter(User.id == id).first()
-
-def get_user_by_email(email: str, db: Session = get_db()):
-    return db.query(User).filter(User.email == email).first()
-
-def get_all_users(skip: int = 0, limit: int = 100, db: Session = get_db()):
-    return db.query(User).offset(skip).limit(limit).all()
-
-def create_user(user: user_schema.UserCreate, db: Session = get_db()):
+def create_user(user: user_schema.UserCreateOrUpdate, db: Session = get_db()):
     user = User(
         email=user.email,
         password=Password(
@@ -32,9 +22,30 @@ def create_user(user: user_schema.UserCreate, db: Session = get_db()):
     db.commit(); db.refresh(user)
     return user
 
-def delete_user(user: user_schema.UserDelete, db: Session = get_db()):
-    user = db.query(User).filter(User.email == user.email and User.password == user.password).first()
+def get_user_by_id(id: UUID, db: Session = get_db()):
+    return db.query(User).filter(User.id == id).first()
 
+def get_user_by_email(email: str, db: Session = get_db()):
+    return db.query(User).filter(User.email == email).first()
+
+def get_all_users(skip: int = 0, limit: int = 100, db: Session = get_db()):
+    return db.query(User).offset(skip).limit(limit).all()
+
+def update_user(user: user_schema.UserCreateOrUpdate, db: Session = get_db()):
+    user = User(
+        email=user.email,
+        password = db.query(Password).filter(Password.id == User.id),
+        username=user.username,
+        nickname=user.nickname,
+        authority=user.authority)
+
+    db.query(User).filter(User.email == user.email).update(user)
+    db.commit()
+    return user
+
+def delete_user(email: str, db: Session = get_db()):
+    user = db.query(User).filter(User.email == email).first()
+    
     db.delete(user)
     db.commit()
     return user
